8e5534fdd90263b425aa0be10d6f3a664cd13c48
kamil-jodlowski <kamil.jodlowski.work@gmail.com>
2024-07-24 14:00:25 +0200

    Update query.sql

--- Query/query.sql
+++ Query/query.sql
@@ -1,1 +1,16 @@
+-- Histogram of Tweets [Twitter SQL Interview Question]
+
+WITH qwerty AS (SELECT 
+user_id AS tweet_bucket,
+COUNT(tweet_id) AS users_num 
+FROM tweets
+WHERE tweet_date BETWEEN '2022-01-01' 
+  AND '2022-12-31'
+GROUP BY user_id) 
+
+SELECT COUNT(tweet_bucket) AS tweet_bucket, users_num 
+FROM qwerty
+GROUP BY users_num
+ORDER BY tweet_bucket ASC;
+
 
